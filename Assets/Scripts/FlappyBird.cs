12b3bd55535c9b19d24c00ecd9633d4feedda9f7
Röttger <mona.roettger@haw-hamburg.de>
2016-10-07 16:11:38 +0200

    start flappy bird

---
+++ Assets/Scripts/FlappyBird.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlappyBird : MonoBehaviour {
+	public float jumpforce = 40f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		detectInput ();
+	}
+
+	void detectInput(){
+		if (Input.GetKeyDown ("space")) {
+			Debug.Log ("Button pressed");
+			jump ();
+		}
+	}
+
+	void jump(){
+		gameObject.GetComponent<Rigidbody> ().velocity = new Vector3 (0f, 0f, 0f);
+		gameObject.GetComponent<Rigidbody>().AddForce (new Vector3 (0f, jumpforce, 0f));
+	}
+}
