22caaf79310a1c1e71e2bd2b3be418f8c6605160
Röttger <mona.roettger@gmail.com>
2016-10-19 16:05:11 +0200

    Script FlappyBird replaced by Player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    //Variablen
+    public Vector2 jumpForce = new Vector2(0, 300);
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        jump();
+	}
+
+    void jump() {
+        if (Input.GetKeyDown("space")) {
+            GetComponent<Rigidbody>().velocity = Vector2.zero;
+            GetComponent<Rigidbody>().AddForce(jumpForce);
+        }
+    }
+}
