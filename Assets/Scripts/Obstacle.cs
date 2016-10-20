69b775df406b9b067310392dcaea156e7b08ac08
Röttger <mona.roettger@gmail.com>
2016-10-20 23:10:17 +0200

    addes randomness an save the scene

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -5,11 +5,13 @@ public class Obstacle : MonoBehaviour {
 
     //Variable
     public Vector3 velocity = new Vector3(-4, 0, 0);
+    public float range = 4;
 
 	// Use this for initialization
 	void Start () {
         GetComponent<Rigidbody>().velocity = velocity;
-      
+        transform.position = new Vector3(transform.position.x, transform.position.y - range * Random.value, transform.position.z);
+
     }
 	
 	// Update is called once per frame
