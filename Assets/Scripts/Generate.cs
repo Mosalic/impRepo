09bdd82ec70e113b982827dff67f5c27be00e3cf
Röttger <mona.roettger@haw-hamburg.de>
2016-10-20 10:35:20 +0200

    improvements (without folder)

---
+++ Assets/Scripts/Generate.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Generate : MonoBehaviour {
+
+	public GameObject pillars;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("CreateObstacle", 1f, 2f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void CreateObstacle(){
+		Instantiate (pillars);
+	}
+}
